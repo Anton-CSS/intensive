e4699c5bba66f5f5708408fc08b92d26d94f0a4c
Anton Rokunets <9251580262@mail.ru>
2020-09-30 14:00:59 +0300

    days_01DZ

--- интенсив/js/main.js
+++ интенсив/js/main.js
@@ -2,9 +2,14 @@
 
 const btnBurger = document.querySelector('.btn-burger'),
       btnClose = document.getElementById('hnf-menu-close-btn'),
-      overlay = document.querySelector('.overlay'),
+      subcatalog = document.querySelector('.subcatalog'),
+      subcatalogHeader = document.querySelector('.subcatalog-header'),
+      btnReturn = document.querySelector('.btn-return'),
       catalog = document.querySelector('.catalog');
 
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay)
 const openMenu = () => {
     catalog.classList.add('open');
     overlay.classList.add('active');
@@ -13,13 +18,30 @@ const openMenu = () => {
 const closeMenu = () =>{
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    closeSubMenu();
 }
+
+const openSubMenu = (event) =>{
+  event.preventDefault();
+  const itemList = event.target.closest('.catalog-list__item');
+  if(itemList){
+    subcatalogHeader.innerHTML = event.target.innerHTML;
+    subcatalog.classList.add('subopen');
+  }
+};
+
+const closeSubMenu = () =>{
+  subcatalog.classList.remove('subopen');
+};
+
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
+btnReturn.addEventListener('click', closeSubMenu);
 document.addEventListener('keydown', (event) =>{
     if(event.code === 'Escape'){
         closeMenu();
     }
 })
+catalog.addEventListener('click', openSubMenu);
