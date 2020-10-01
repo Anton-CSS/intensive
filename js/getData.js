edb04eead9894c27a9b57222e1a6a68418078290
Anton Rokunets <9251580262@mail.ru>
2020-10-01 15:20:00 +0300

    days_02

---
+++ js/getData.js
@@ -0,0 +1,55 @@
+const PARAM = {
+  cat: 'category',
+  subcat: 'subcategory',
+  serch: ['category', 'subcategory', 'name', 'description', ]
+}
+
+
+export const getData = {
+  url: 'database/dataBase.json',
+  get(process){
+    fetch(this.url)
+      .then(response => response.json())
+      .then(process)
+  },
+  wishLis(list, callback){
+    this.get((data) => {
+      const result = data.filter(item => list.includes(item.id));
+      callback(result);
+    })
+  },
+
+  item(value, callback){
+    this.get((data) => {
+      const result = data.find(item => item.id === value);
+      callback(result);
+    })
+  },
+
+  cart(list, callback){
+    this.get((data) => {
+      const result = data.filter(item => list.som(obj => obj.id === item.id));
+      callback(result);
+    })
+  },
+
+  category(prop, value, callback){
+    this.get((data) => {
+      const result = data.filter(item => item[PARAM[prop]].toLowerCase() === value.toLowerCase());
+      callback(result);
+    })
+  },
+
+  serch(value, callback){
+    this.get((data) => {
+      const result = data.filter(item => {
+        for (let key in item){
+            if( PARAM.serch.includes(key) && item[key].toLowerCase().includes(value.toLowerCase()) ){
+              return true;
+            }
+        } 
+      });
+      callback(result);
+    })
+  }
+}
